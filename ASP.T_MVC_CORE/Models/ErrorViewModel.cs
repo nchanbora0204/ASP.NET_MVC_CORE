eea579f6a4952ac31cf3fcca8f4d30872ce8884e
nchanbora0204 <namtrung0204@gmail.com>
2023-12-24 14:42:18 +0700

    khoi tao

---
+++ ASP.T_MVC_CORE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.T_MVC_CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
